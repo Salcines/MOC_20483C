85e0fdbebcc8ea7be449e7d5dae384880e18ab11
Francisco Salcines <fransalcines@hotmail.com>
2017-12-11 03:00:45 +0100

    Exercise 2 end Solution

--- MOC20483Labs/Mod02Labs/Exercise 2/School/StudentForm.xaml.cs
+++ MOC20483Labs/Mod02Labs/Exercise 2/School/StudentForm.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 
 namespace School
@@ -20,13 +21,43 @@ namespace School
         // If the user clicks OK to save the Student details, validate the information that the user has provided
         private void ok_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: Exercise 2: Task 2a: Check that the user has provided a first name
+
+           // TODO: Exercise 2: Task 2a: Check that the user has provided a first name
+            if (this.firstName.Text == String.Empty)
+            {
+                MessageBox.Show("The student must have a first name", "Error",
+                   MessageBoxButton.OK,
+                   MessageBoxImage.Error, MessageBoxResult.OK);
+                return;
+            }
             // TODO: Exercise 2: Task 2b: Check that the user has provided a last name
+            if (String.IsNullOrEmpty(this.lastName.Text))
+            {
+                MessageBox.Show("The student must have a last name", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error, MessageBoxResult.OK);
+                return;
+            }
             // TODO: Exercise 2: Task 3a: Check that the user has entered a valid date for the date of birth
+            if (!DateTime.TryParse(this.dateOfBirth.Text, out DateTime validateDate))
+            {
+                MessageBox.Show("The date of birth must be a valid date", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error, MessageBoxResult.OK);
+                return;
+            }
             // TODO: Exercise 2: Task 3b: Verify that the student is at least 5 years old
+            TimeSpan validateAge = DateTime.Now.Subtract(validateDate); 
+            int age = (int)(validateAge.Days/365.25);
+            if (age < 5)
+            {
+                MessageBox.Show("The age must at least 5 years old", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
 
             // Indicate that the data is valid
-            this.DialogResult = true;
+            
+                this.DialogResult = true;
+            
         }
     }
 }
